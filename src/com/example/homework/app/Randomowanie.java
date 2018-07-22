9948ff23acd2f9cc5347c52a6984ccc3983e69b4
Magda Kubin <m.mielnicka79@gmail.com>
2018-07-22 19:03:17 +0200

    Rozwiązanie zadania

---
+++ src/com/example/homework/app/Randomowanie.java
@@ -0,0 +1,27 @@
+package com.example.homework.app;
+
+import java.util.Random;
+
+public class Randomowanie {
+
+    public static void main(String[] args) {
+
+        int[] tab = new int[5];
+
+        Random random =new Random();
+        int i;
+        for (i = 0; i < tab.length; i++) {
+
+            tab[i] = random.nextInt(100);
+            System.out.println(tab[i]);
+        }
+
+            for (i = tab.length-1;i >= 0;i--){
+               System.out.println(tab[i]);
+        }
+
+
+        }
+
+    }
+
